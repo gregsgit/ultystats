8604d3daf17db7ce73c460d272d97aa45b8fd831
Greg Sullivan <gregs@sulliwood.org>
2017-09-26 20:20:09 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# ultystats
+# Stats json format
+
+A stats json file looks like
+
+```
+{ "game" : {
+    "home" : "Huckagenerians",
+    "versus" : "KitKat",
+    "date" : "9/15/2017",
+    "plays" : [
+	...
+	]
+  }
+}
+```
+
+## Plays
+
+Each play is one of:
+
+1. 'V' all by itself, representing the other team scoring
+2. a "note" object in the form { "note" : "string"}
+3. 'T' meaning a timeout
+4. a sequence of throws
+
+Each element in a sequence of throws is one of:
+
+1. "D-name", to start a sequence, means "name" made a defensive play
+   to regain possession. 
+2. "name", a player receiving, and then throwing, the disk. 
+3. "X", to finish a sequence, means the name immediately preceding
+   threw it away (bad throw). 
+4. "name-D", to end a sequence, means the throw (from the preceding
+   name) was reasonably good, but "name" dropped it.
+5. "name-!", to end a sequence, means "name" scored by recieving a
+   throw in the endzone. 
+
+Examples:
+
+* `[D-Adam, Debbie, Chris, ...]` means Adam made D, Debbie picked up
+  and threw to Chris 
+* `[D-Adam, Adam, X]` then means Adam made D then threw it away
+* `[D-Adam, Adam, X], {"note: "Adam was throwing to Debbie"}` then
+  means Adam made D then tried to throw to Debbie but throw was
+  incomplete. 
+* `[..., Debbie, Adam-!]` means Debbie threw to Adam for score
+* `[..., Debbie, Adam-D]` means Debbie threw a decent throw to Adam,
+  but Adam dropped it.
+
---
+++ stats-decoder.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+import csv
+import json
+import sys                      # for .exit()
+import re
+
+players = {}
+
+def incr_player(player, key):
+    if not player in players:
+        players[player] = { 'name' : player, 'ds' : 0, 
+                            'throws' : 0, 'bad_throws' : 0, 
+                            'catches' : 0, 'drops' : 0, 
+                            'assists' : 0, 'scores' : 0}
+        players[player][key] = 1
+    else:
+        players[player][key] = players[player][key] + 1
+
+def increment_ds(player):
+    incr_player(player, 'ds')
+
+def increment_throws(player):
+    incr_player(player, 'throws')
+    
+def increment_bad_throws(player):
+    incr_player(player, 'bad_throws')
+
+def increment_catches(player):
+    incr_player(player, 'catches')
+
+def increment_drops(player):
+    incr_player(player, 'drops')
+
+def increment_assists(player):
+    incr_player(player, 'assists')
+
+def increment_scores(player):
+    incr_player(player, 'scores')
+
+
+def print_player(name, stats):
+    print("{}:".format(name))
+    print("\tDefensive plays: {}".format(stats['ds']))
+    print("\tthrows: {}".format(stats['throws']))
+    print("\tbad_throws: {}".format(stats['bad_throws']))
+    if stats['throws'] > 0:
+        print("\tthrow percentage: {}".format(int(stats['bad_throws']) / int(stats['throws']) * 100))
+    print("\tcatches: {}".format(stats['catches']))
+    print("\tdrops: {}".format(stats['drops']))
+    if stats['catches'] > 0:
+        print("\tcatch percentage: {}".format(int(stats['catches']) / (int(stats['catches']) +
+                                                                     int(stats['drops'])) * 100))
+    print("\tassists: {}".format(stats['assists']))
+    print("\tscores: {}".format(stats['scores']))
+
+def usage():
+    print("""Usage: {} <filename>.
+       will read <filename>.json according to format described in README, 
+       and will generate <filename>.csv with rows for each player and
+       columns for defensive plays, throws, bad throws, % bad throws, drops, 
+       % drops, assists, and scores.
+    """.format(argv[0]))
+    sys.exit()
+
+def main(fname):
+    with open("{}.json".format(fname), 'r') as f:
+        data = json.load(f)
+        plays = data['game']['plays']
+        us_score = 0
+        them_score = 0
+        for play in plays:
+            if isinstance(play, dict) and 'note' in play:
+                print("note: {}".format(play['note']))
+            elif play == "V":
+                them_score = them_score + 1
+            elif play == "T":
+                print("timeout")
+            elif isinstance(play, list):
+                tcount = 0          # number of throws
+                player = ""
+                for t in play:
+                    tcount = tcount + 1
+                    prev = player
+                    if isinstance(t, dict) and 'note' in t:
+                        print("play note: {}".format(t['note']))
+                    elif not isinstance(t, str):
+                        # what is this? not a string
+                        print("Unidentified element (not a string) of play sequence: {}".format(t))
+                        sys.exit()
+                    # defensive play:
+                    elif tcount == 1 and re.match("D-(.*)", t):
+                        player = re.match("D-(.*)", t).group(1)
+                        increment_ds(player)
+                    # score: 
+                    elif tcount == len(play) and re.match("(.*)-!", t):
+                        player = re.match("(.*)-!", t).group(1)
+                        increment_scores(player)
+                        increment_assists(prev)
+                        us_score = us_score + 1
+                    # drop:
+                    elif tcount == len(play) and re.match("(.*)-D", t):
+                        player = re.match("(.*)-D", t).group(1)
+                        increment_drops(player)
+                    # bad throw:
+                    elif tcount == len(play) and t == "X":
+                        increment_bad_throws(prev)
+                    # a receiver who then throws
+                    else:
+                        player = t
+                        increment_catches(player)
+                        increment_throws(player)
+
+    print("Final score: Us = {}, Them = {}.".format(us_score, them_score))
+
+
+    with open("{}.csv".format(fname), 'w') as csvfile:
+        fieldnames = ['name', 'ds', 'throws', 'bad_throws', 'throw_percentage', 
+                      'catches', 'drops', 'catch_percentage', 'assists', 'scores']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for name, stats in players.items():
+            # print_player(name, stats)
+            if stats['throws'] > 0:
+                stats['throw_percentage'] = (stats['bad_throws'] / stats['throws']) * 100
+            else:
+                stats['throw_percentage'] = 0
+            if stats['catches'] > 0:
+                stats['catch_percentage'] = (stats['catches'] / (stats['catches'] + 
+                                                                 stats['drops'])) * 100
+            else:
+                stats['catch_percentage'] = 0
+            writer.writerow(stats)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        usage()                 # exits
+    main(sys.argv[1])
